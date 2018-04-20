a07b9298afb6a27eae06d3436c8ba9232a9387a9
Nicholas Halloran <nmhalloran@gmail.com>
2018-04-20 14:59:49 -0700

    Add DELTAS to KnigthPathFinder

--- polytree/skeleton/lib/knight.rb
+++ polytree/skeleton/lib/knight.rb
@@ -2,5 +2,24 @@ require 'pry'
 require 'byebug'
 require_relative '00_tree_node'
 
-class Knight
+class KnightPathFinder
+  
+  DELTA = [
+            [-2,-1],
+            [-1,-2],
+            [-2, 1],
+            [-1, 2],
+            [ 1, 2],
+            [ 2, 1],
+            [ 2,-1],
+            [ 1,-2]
+          ]
+  
+  attr_accessor :start_pos
+  
+  def initialize(start_pos=[0,0])
+    @start_pos = start_pos
+  end
+  
+  
 end
