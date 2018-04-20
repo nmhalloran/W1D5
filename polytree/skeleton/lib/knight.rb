935183552014bdcd1580f5a910e8eec237fc5fca
Nicholas Halloran <nmhalloran@gmail.com>
2018-04-20 15:39:16 -0700

    Finish phase1 of KnightPathFinder

--- polytree/skeleton/lib/knight.rb
+++ polytree/skeleton/lib/knight.rb
@@ -15,11 +15,35 @@ class KnightPathFinder
             [ 1,-2]
           ]
   
-  attr_accessor :start_pos
+  attr_accessor :pos
   
-  def initialize(start_pos=[0,0])
-    @start_pos = start_pos
+  def initialize(pos=[0,0])
+    @pos = pos
+    @visited_pos = [pos]
   end
   
+  def valid_moves
+    DELTA.select do |move|
+      (pos.first + move[0]).between?(0, 7) &&
+      (pos.last + move[1]).between?(0, 7)
+    end
+  end
+  
+  def new_move_pos
+    all_moves = valid_moves.map do |move| 
+      [move[0] + pos[0], move[1] + pos[1]]
+    end 
+    new_moves = all_moves.reject { |move| @visited_pos.include?(move) }
+    @visited_pos += new_moves 
+    new_moves
+  end 
+  
+  def build_move_tree
+        
+  end
   
 end
+
+a = KnightPathFinder.new([3, 3])
+# p a.valid_moves
+p a.new_move_pos
